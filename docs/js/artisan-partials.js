79e14ec2f68e217523c1a2cc7b0e62dfe81e63d8
Luca Zampetti <lzampetti@gmail.com>
2017-11-07 08:08:30 +0100

    artisan

--- docs/js/artisan-partials.js
+++ docs/js/artisan-partials.js
@@ -118,11 +118,11 @@ $templateCache.put("artisan/modals/modals","<div class=\"modals\" ng-class=\"{ v
     "</div>")
 
 $templateCache.put("artisan/nav/nav-item","<a class=\"nav-link\" href=\"#\" ng-href=\"{{item.$nav.link}}\" ng-bind=\"item.name\"></a>\n" +
-    "<ul class=\"nav nav-{{item.$nav.level}} flex-column\" ng-if=\"item.items\">\n" +
+    "<ul class=\"nav nav-{{item.$nav.level}}\" ng-if=\"item.items\">\n" +
     "    <li class=\"nav-item\" ng-class=\"item.$nav.state\" nav-item=\"item\" ng-repeat=\"item in item.items track by $index\"></li>\n" +
     "</ul>")
 
-$templateCache.put("artisan/nav/nav","<ul class=\"nav nav-{{item.$nav.level}} flex-column\" ng-if=\"item.items\">\n" +
+$templateCache.put("artisan/nav/nav","<ul class=\"nav nav-{{item.$nav.level}}\" ng-if=\"item.items\">\n" +
     "    <li class=\"nav-item\" ng-class=\"item.$nav.state\" nav-item=\"item\" ng-repeat=\"item in item.items track by $index\"></li>\n" +
     "</ul>")
 }]);
