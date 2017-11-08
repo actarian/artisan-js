5baddd527449c8a2b45f889c5b7ff571801135c1
Luca Zampetti <lzampetti@gmail.com>
2017-11-08 06:27:19 +0100

    artisan

--- docs/js/artisan.js
+++ docs/js/artisan.js
@@ -1078,52 +1078,6 @@
     }]);
 
 }());
-/* global angular */
-
-(function() {
-    "use strict";
-
-    var app = angular.module('artisan');
-
-    app.directive('ngClick', ['Events', function(Events) {
-        return {
-            restrict: 'A',
-            priority: 0,
-            link: link
-        };
-
-        function link(scope, element, attributes, model) {
-            element.addClass('material');
-            var material = document.createElement('material');
-            element[0].appendChild(material);
-
-            function onClick(e) {
-                element.removeClass('animate');
-                void element.offsetWidth;
-                // material.style.animationPlayState = "paused";
-                material.style.left = e.relative.x + 'px';
-                material.style.top = e.relative.y + 'px';
-                setTimeout(function() {
-                    element.addClass('animate');
-                    setTimeout(function() {
-                        element.removeClass('animate');
-                    }, 1000);
-                }, 10);
-            }
-
-            var listeners = {
-                click: onClick,
-            };
-            var events = new Events(element).add(listeners);
-
-            scope.$on('$destroy', function() {
-                events.remove(listeners);
-            });
-
-        }
-    }]);
-
-}());
 /* global angular, app, Autolinker */
 
 (function() {
@@ -1791,24 +1745,51 @@
 
 }());
 /* global angular */
+(function () {
+	"use strict";
+	var app = angular.module('artisan');
+	// micro interactions
+	function miTap(Events) {
+		return {
+			restrict: 'A',
+			priority: 0,
+			link: link
+		};
 
-(function() {
-    "use strict";
+		function link(scope, element, attributes, model) {
+			if (attributes.href === '#' && !attributes.ngClick) {
+				return;
+			}
+			element.addClass('miTap');
+			var material = document.createElement('mi-tap');
+			element[0].appendChild(material);
 
-    var app = angular.module('artisan');
-
-    app.directive('header', [function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'artisan/header/header',
-            transclude: {
-                'header': '?headerItems',
-            },
-            link: function(scope, element, attributes, model) {}
-        };
-    }]);
-
+			function onClick(e) {
+				element.removeClass('animate');
+				void element.offsetWidth;
+				// material.style.animationPlayState = "paused";
+				material.style.left = e.relative.x + 'px';
+				material.style.top = e.relative.y + 'px';
+				setTimeout(function () {
+					element.addClass('animate');
+					setTimeout(function () {
+						element.removeClass('animate');
+					}, 1000);
+				}, 10);
+			}
+			var listeners = {
+				click: onClick,
+			};
+			var events = new Events(element).add(listeners);
+			scope.$on('$destroy', function () {
+				events.remove(listeners);
+			});
+		}
+	}
+	app.directive('href', ['Events', miTap]);
+	app.directive('ngClick', ['Events', miTap]);
 }());
+
 /* global angular */
 
 (function() {
